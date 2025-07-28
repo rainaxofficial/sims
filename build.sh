30a595fb3dd006e414e5dadbfb7bd9f218c01ae0
rainaxofficial <rainaxofficial@gmail.com>
2025-07-28 11:34:46 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,17 +3,27 @@ set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
+# Download and extract Chrome (cached)
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src || cd $HOME/project # fallback if /src doesn't exist
+  wget -q -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x chrome.deb $STORAGE_DIR/chrome
+  rm chrome.deb
+  cd $HOME/project/src || cd $HOME/project
 else
-  echo "...Using Chrome from cache"
+  echo "...Using cached Chrome"
 fi
 
-# Install Python packages
+# Download and install matching ChromeDriver v138
+echo "...Downloading ChromeDriver v138"
+cd /opt/render/project/.render
+wget -q -O chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/138.0.7204.168/linux64/chromedriver-linux64.zip
+unzip -q chromedriver.zip
+mv chromedriver-linux64/chromedriver .
+chmod +x chromedriver
+rm -rf chromedriver.zip chromedriver-linux64
+
+# Install Python dependencies
 pip install -r requirements.txt
