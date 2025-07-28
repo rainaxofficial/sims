4677ee1f10ccc91e18ae82610f023c33bfc44229
rainaxofficial <rainaxofficial@gmail.com>
2025-07-28 11:35:39 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Point Selenium to installed Chrome binary
+# Set Chrome binary and driver path
 export CHROME_BIN="/opt/render/project/.render/chrome/opt/google/chrome/google-chrome"
-export PATH="$CHROME_BIN:$PATH"
+export PATH="/opt/render/project/.render:$PATH"
 
+# Launch app
 gunicorn app:app --bind 0.0.0.0:$PORT
