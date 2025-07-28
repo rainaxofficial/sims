b023927841651d30bcdf978b2caf2cb90a4d842f
rainaxofficial <rainaxofficial@gmail.com>
2025-07-28 11:26:36 +0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o errexit
+
+STORAGE_DIR=/opt/render/project/.render
+
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src || cd $HOME/project # fallback if /src doesn't exist
+else
+  echo "...Using Chrome from cache"
+fi
+
+# Install Python packages
+pip install -r requirements.txt
